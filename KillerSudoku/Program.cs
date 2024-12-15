f60d8fa72ae200698874bb78bcb55d5a7e3cafc3
njomzaarexhepii@gmail.com <75095687+NjomzaRexhepi@users.noreply.github.com>
2024-12-15 23:30:40 +0100

    changes

--- KillerSudoku/Program.cs
+++ KillerSudoku/Program.cs
@@ -24,8 +24,13 @@ namespace KillerSudoku
 
             List<Cage> cages = new List<Cage>
         {
+           
             new Cage(15, new List<(int, int)> { (0, 0), (0, 1), (1, 0) }),
             new Cage(10, new List<(int, int)> { (0, 2), (1, 1), (1, 2) }),
+            new Cage(20, new List<(int, int)> { (2, 0), (2, 1), (2, 2) }),
+            new Cage(7, new List<(int, int)> { (3, 0), (3, 1) }),
+            new Cage(16, new List<(int, int)> { (4, 4), (5, 4), (6, 4) }),
+            new Cage(18, new List<(int, int)> { (8, 7), (8, 8), (7, 8) }),
         };
 
             if (KillerSudokuClass.SolveKillerSudoku(grid, cages))
