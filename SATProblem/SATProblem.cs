dea194d0b69563883ca86e2ae05425193e7095ab
Kaltrinë Baliu <kaltrine.baliu@student.uni-pr.edu>
2024-12-16 17:56:58 +0100

    added variable that defines sat problem

--- SATProblem/SATProblem.cs
+++ SATProblem/SATProblem.cs
@@ -6,7 +6,13 @@ namespace SATProblemNamespace
 {
     public class SATProblem
     {
-        //   
+        private int numGuests;
+        private int numTables;
+        private int seatsPerTable;
+        private List<(int, int)> cannotSitTogether;
+        private List<(int, int)> mustSitTogether;
+
+
     }
 }
 
