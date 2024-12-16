f4e5bcac0e60cf1e10b71d3e3b81af43b850889f
Kaltrinë Baliu <kaltrine.baliu@student.uni-pr.edu>
2024-12-16 17:38:43 +0100

    SAT project created

---
+++ SATProblem/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using SATProblemNamespace;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+
---
+++ SATProblem/SATProblem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.OrTools.Sat;
+
+namespace SATProblemNamespace
+{
+    public class SATProblem
+    {
+        //   
+    }
+}
+
