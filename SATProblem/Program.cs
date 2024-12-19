13c59488b641afb6b08498e1072ffc8d6c2f713c
Kaltrinë Baliu <kaltrine.baliu@student.uni-pr.edu>
2024-12-19 23:29:14 +0100

    added the input for the guests pair that can not sit together

--- SATProblem/Program.cs
+++ SATProblem/Program.cs
@@ -12,6 +12,20 @@ class Program
 
         var problem = new SATProblem(numGuests, numTables, seatsPerTable);
 
+        Console.WriteLine("Jepni numrin e çifteve të mysafirëve që NUK mund të ulen së bashku:");
+        int notTogetherCount = int.Parse(Console.ReadLine() ?? "0");
+        for (int i = 0; i < notTogetherCount; i++)
+        {
+            Console.Write($"Çifti {i + 1} (jepni si format M1 M2): ");
+            var input = Console.ReadLine()?.Split() ?? new string[0];
+            if (input.Length < 2) continue;
+
+            int guestA = int.Parse(input[0][1..]);
+            int guestB = int.Parse(input[1][1..]);
+            problem.AddCannotSitTogether(guestA, guestB);
+        }
+
+
 
 
     }
