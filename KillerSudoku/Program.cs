9cca7140166db7c0643e7c8b9d9bf81501f30b57
erblinaberisha <Ereblina.Berisha@student.uni-pr.edu>
2024-12-25 20:47:47 +0100

    killer sudoku with 3 levels

--- KillerSudoku/Program.cs
+++ KillerSudoku/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KillerSudoku
 {
@@ -10,31 +7,36 @@ namespace KillerSudoku
     {
         public static void Main(string[] args)
         {
-            int[,] grid = {
+            Console.WriteLine("Select Difficulty Level (1: Easy, 2: Medium, 3: Hard):");
+            int difficultyLevel = int.Parse(Console.ReadLine());
 
-            { 5, 0, 0, 0, 0, 0, 0, 1, 0 },
-            { 0, 0, 0, 0, 0, 3, 0, 0, 4 },
-            { 0, 0, 0, 0, 8, 0, 0, 0, 0 },
-            { 0, 0, 4, 0, 0, 0, 0, 0, 0 },
-            { 0, 6, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 7, 0, 0, 0 },
-            { 0, 0, 0, 0, 9, 0, 0, 0, 0 },
-            { 0, 0, 0, 4, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 5, 0 }
+            int[,] grid;
+            List<Cage> cages;
 
-        };
+            // Setup grid and cages based on difficulty
+            switch (difficultyLevel)
+            {
+                case 1:
+                    grid = GetEasyGrid();
+                    cages = GetEasyCages();
+                    break;
 
-            List<Cage> cages = new List<Cage>
-        {
+                case 2:
+                    grid = GetMediumGrid();
+                    cages = GetMediumCages();
+                    break;
 
-            new Cage(15, new List<(int, int)> { (0, 0), (0, 1), (1, 0) }),
-            new Cage(10, new List<(int, int)> { (0, 2), (1, 1), (1, 2) }),
-            new Cage(20, new List<(int, int)> { (2, 0), (2, 1), (2, 2) }),
-            new Cage(7, new List<(int, int)> { (3, 0), (3, 1) }),
-            new Cage(16, new List<(int, int)> { (4, 4), (5, 4), (6, 4) }),
-            new Cage(18, new List<(int, int)> { (8, 7), (8, 8), (7, 8) }),
-        };
+                case 3:
+                    grid = GetHardGrid();
+                    cages = GetHardCages();
+                    break;
 
+                default:
+                    Console.WriteLine("Invalid difficulty level.");
+                    return;
+            }
+
+            // Solve the Killer Sudoku puzzle
             if (KillerSudokuClass.SolveKillerSudoku(grid, cages))
             {
                 Console.WriteLine("Killer Sudoku solved successfully:");
@@ -44,6 +46,96 @@ namespace KillerSudoku
             {
                 Console.WriteLine("No solution found for the given Killer Sudoku puzzle.");
             }
+        }
+
+        // Easy Grid and Cages
+        static int[,] GetEasyGrid()
+        {
+            return new int[9, 9]
+            {
+                { 5, 0, 0, 0, 0, 0, 0, 1, 0 },
+                { 0, 0, 0, 0, 0, 3, 0, 0, 4 },
+                { 0, 0, 0, 0, 8, 0, 0, 0, 0 },
+                { 0, 0, 4, 0, 0, 0, 0, 0, 0 },
+                { 0, 6, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 7, 0, 0, 0 },
+                { 0, 0, 0, 0, 9, 0, 0, 0, 0 },
+                { 0, 0, 0, 4, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 5, 0 }
+            };
+        }
+
+        static List<Cage> GetEasyCages()
+        {
+            return new List<Cage>
+            {
+                new Cage(15, new List<(int, int)> { (0, 0), (0, 1), (1, 0) }),
+                new Cage(10, new List<(int, int)> { (0, 2), (1, 1), (1, 2) }),
+                new Cage(20, new List<(int, int)> { (2, 0), (2, 1), (2, 2) }),
+                new Cage(7, new List<(int, int)> { (3, 0), (3, 1) }),
+                new Cage(16, new List<(int, int)> { (4, 4), (5, 4), (6, 4) }),
+                new Cage(18, new List<(int, int)> { (8, 7), (8, 8), (7, 8) })
+            };
+        }
+
+        // Medium Grid and Cages
+        static int[,] GetMediumGrid()
+        {
+            return new int[9, 9]
+            {
+                { 0, 3, 0, 0, 0, 0, 0, 9, 0 },
+                { 0, 0, 0, 9, 0, 0, 0, 0, 4 },
+                { 0, 0, 7, 0, 5, 0, 0, 8, 0 },
+                { 0, 6, 0, 0, 0, 0, 0, 0, 3 },
+                { 9, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 5, 0, 0, 7, 0, 0 },
+                { 0, 9, 0, 7, 3, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 7, 2 },
+                { 7, 0, 0, 0, 0, 0, 9, 0, 0 }
+            };
+        }
+
+        static List<Cage> GetMediumCages()
+        {
+            return new List<Cage>
+            {
+                new Cage(20, new List<(int, int)> { (0, 1), (1, 0), (1, 1), (1, 2) }),
+                new Cage(15, new List<(int, int)> { (2, 3), (2, 4), (3, 1) }),
+                new Cage(10, new List<(int, int)> { (4, 0), (5, 0), (5, 1) }),
+                new Cage(24, new List<(int, int)> { (6, 1), (6, 2), (7, 4), (7, 3) }),
+                new Cage(16, new List<(int, int)> { (8, 0), (7, 0), (6, 0) }),
+                new Cage(18, new List<(int, int)> { (5, 3), (6, 4) })
+            };
+        }
+
+        // Hard Grid and Cages
+        static int[,] GetHardGrid()
+        {
+            return new int[9, 9]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+        }
+
+        static List<Cage> GetHardCages()
+        {
+            return new List<Cage>
+            {
+                new Cage(15, new List<(int, int)> { (0, 0), (0, 1), (1, 0) }),
+                new Cage(10, new List<(int, int)> { (0, 2), (1, 1), (1, 2) }),
+                new Cage(20, new List<(int, int)> { (2, 0), (2, 1), (2, 2) }),
+                new Cage(7, new List<(int, int)> { (3, 0), (3, 1) }),
+                new Cage(16, new List<(int, int)> { (4, 4), (5, 4), (6, 4) }),
+                new Cage(18, new List<(int, int)> { (8, 7), (8, 8), (7, 8) }),
+            };
         }
     }
 }
